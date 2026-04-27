765c7ea20695011a3abd3e72bffc7d5e06fb38eb
Aahil Khan <aahil0116@gmail.com>
2026-04-27 13:49:19 +0530

    Delete sanjivini directory

--- sanjivini/src/shared/navigation/menuData.js
+++
@@ -1,8 +0,0 @@
-export const primaryMenu = [
-  { label: "Home", path: "/" },
-  { label: "Facilities & Services", path: "/facilities-and-services" },
-  { label: "Specialties", path: "/specialties" },
-  { label: "Our Chronicles", path: "/our-chronicles" },
-  { label: "TPA's & Empanelments", path: "/tpas-and-empanelments" },
-  { label: "Contact Us", path: "/contact-us" }
-];
